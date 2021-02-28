1373e5ac4c23e4468f999c7225613e635d9f1257
puifaiba <puifai.ba@gmail.com>
2021-02-27 19:09:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector("#create-task-form");
+  const newTaskInput = document.querySelector("#new-task-description");
+  const taskList = document.querySelector("#tasks");
+
+  taskForm.addEventListener("submit", addTask);
+
+  function addTask(event) {
+    event.preventDefault();
+
+    task = document.createElement("li");
+    task.textContent = newTaskInput.value;
+    taskList.append(task);
+
+    event.target.reset();
+  }
 });
